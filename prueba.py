e60cc92883788afe1904b471c8e2312d2b5e3a34
juanalonso21 <juanalonsopulpi@gmail.com>
2025-03-25 12:06:36 +0100

    Commit señalizando las librerias que se van a utilizar

--- prueba.py
+++ prueba.py
@@ -1,1 +1,12 @@
 print("Hello World")
+# Librerias instaladas
+# pip install pandas numpy scipy
+# Manipulacion de datos 
+# pip install matplotlib seaborn
+# Visualizacion de datos
+# pip install scikit-learn
+# Para regresión, clasificación y clustering.
+# pip install tensorflow
+# Modelos más avanzados (redes neuronales)opcional
+# pip install requests beautifulsoup4 selenium
+# Web scraping
